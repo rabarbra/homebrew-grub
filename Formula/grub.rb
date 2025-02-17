ca1872663cd1590ed52c17836c59cce039a817a8
Polina Simonenko <polina.simonenko@wobcom.de>
2025-02-17 10:00:06 +0100

    first commit

---
+++ Formula/grub.rb
@@ -0,0 +1,47 @@
+class Grub < Formula
+  desc "GNU GRUB bootloader for i386-elf"
+  homepage "https://www.gnu.org/software/grub/"
+  url "https://ftp.gnu.org/gnu/grub/grub-2.06.tar.xz"
+  sha256 "b79ea44af91b93d17cd3fe80bdae6ed43770678a9a5ae192ccea803ebb657ee1"
+  license "GPL-3.0-or-later"
+
+  head do
+    url "https://git.savannah.gnu.org/git/grub.git"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext" => :build
+  depends_on "texinfo" => :build
+  depends_on "help2man" => :build
+  depends_on "gcc" => :build
+  depends_on "binutils" => :build
+  depends_on "make" => :build
+  depends_on "objconv" => :build
+
+  def install
+    mkdir "build" do
+      if build.head?
+        system "../bootstrap"
+        system "../autogen.sh"
+      end
+
+      system "../configure",
+             "--disable-werror",
+             "--target=i386-elf",
+             "--prefix=#{prefix}",
+             "TARGET_CC=i386-elf-gcc",
+             "TARGET_OBJCOPY=i386-elf-objcopy",
+             "TARGET_STRIP=i386-elf-strip",
+             "TARGET_NM=i386-elf-nm",
+             "TARGET_RANLIB=i386-elf-ranlib"
+
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/grub-file", "--is-x86-multiboot", "#{bin}/grub-mkimage"
+  end
+end
