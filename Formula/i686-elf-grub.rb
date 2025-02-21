e51b9a48f71c2471b8b0af6df711567a1ccb64c4
Polina <36280649+rabarbra@users.noreply.github.com>
2025-02-21 19:46:28 +0100

    cleanup

--- Formula/i686-elf-grub.rb
+++ Formula/i686-elf-grub.rb
@@ -5,10 +5,6 @@ class I686ElfGrub < Formula
   sha256 "b79ea44af91b93d17cd3fe80bdae6ed43770678a9a5ae192ccea803ebb657ee1"
   license "GPL-3.0-or-later"
 
-  head do
-    url "https://git.savannah.gnu.org/git/grub.git"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "bison" => :build
@@ -28,19 +24,14 @@ class I686ElfGrub < Formula
     ENV["CFLAGS"] = "-Os -Wno-error=incompatible-pointer-types"
 
     mkdir "build" do
-      if build.head?
-        system "../bootstrap"
-        system "../autogen.sh"
-      end
-
       args = %W[
-        --disable-werror
+        # --disable-werror
         --target=#{target}
         --prefix=#{prefix}/#{target}
         --bindir=#{bin}
         --libdir=#{lib}/#{target}
-        --datarootdir=#{share}/#{target}
-        --sysconfdir=#{etc}/#{target}
+        # --datarootdir=#{share}/#{target}
+        # --sysconfdir=#{etc}/#{target}
         --with-platform=pc
         --program-prefix=#{target}-
         TARGET_CC=#{target}-gcc
@@ -53,16 +44,19 @@ class I686ElfGrub < Formula
       system "../configure", *args
       system "make"
       system "make", "install"
-
     end
   end
 
   test do
     target = "i686-elf"
     (testpath/"boot.c").write <<~C
-      __asm__(".align 4\\n.long 0x1BADB002\\n.long 0x0\\n.long -(0x1BADB002 + 0x0)");
+      __asm__(
+        ".align 4\\n"
+        ".long 0x1BADB002\\n"
+        ".long 0x0\\n"
+        ".long -(0x1BADB002 + 0x0)\\n"
+      );
     C
-
     system Formula["#{target}-gcc"].bin/"#{target}-gcc", "-c", "-o", "boot", "boot.c"
     assert_match "0",
       shell_output("#{bin}/#{target}-grub-file --is-x86-multiboot boot; echo $?")
--- Formula/x86_64-elf-grub.rb
+++ Formula/x86_64-elf-grub.rb
@@ -5,10 +5,6 @@ class X8664ElfGrub < Formula
   sha256 "b79ea44af91b93d17cd3fe80bdae6ed43770678a9a5ae192ccea803ebb657ee1"
   license "GPL-3.0-or-later"
 
-  head do
-    url "https://git.savannah.gnu.org/git/grub.git"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "bison" => :build
@@ -28,19 +24,14 @@ class X8664ElfGrub < Formula
     ENV["CFLAGS"] = "-Os -Wno-error=incompatible-pointer-types"
 
     mkdir "build" do
-      if build.head?
-        system "../bootstrap"
-        system "../autogen.sh"
-      end
-
       args = %W[
-        --disable-werror
+        # --disable-werror
         --target=#{target}
         --prefix=#{prefix}/#{target}
         --bindir=#{bin}
         --libdir=#{lib}/#{target}
-        --datarootdir=#{share}/#{target}
-        --sysconfdir=#{etc}/#{target}
+        # --datarootdir=#{share}/#{target}
+        # --sysconfdir=#{etc}/#{target}
         --with-platform=pc
         --program-prefix=#{target}-
         TARGET_CC=#{target}-gcc
@@ -59,9 +50,13 @@ class X8664ElfGrub < Formula
   test do
     target = "x86_64-elf"
     (testpath/"boot.c").write <<~C
-      __asm__(".align 4\\n.long 0x1BADB002\\n.long 0x0\\n.long -(0x1BADB002 + 0x0)");
+      __asm__(
+        ".align 4\\n"
+        ".long 0x1BADB002\\n"
+        ".long 0x0\\n"
+        ".long -(0x1BADB002 + 0x0)\\n"
+      );
     C
-
     system Formula["#{target}-gcc"].bin/"#{target}-gcc", "-c", "-o", "boot", "boot.c"
     assert_match "0",
       shell_output("#{bin}/#{target}-grub-file --is-x86-multiboot boot; echo $?")
