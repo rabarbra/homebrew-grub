3750cc72ae9d7c60871f3feba3d2f76ae4be25a2
Polina <36280649+rabarbra@users.noreply.github.com>
2025-02-27 12:44:52 +0100

    test

--- Formula/i686-elf-grub.rb
+++ Formula/i686-elf-grub.rb
@@ -1,20 +1,18 @@
 class I686ElfGrub < Formula
   desc "GNU GRUB bootloader for i686-elf"
-  homepage "https://www.gnu.org/software/grub/"
+  homepage "https://savannah.gnu.org/projects/grub"
   url "https://ftp.gnu.org/gnu/grub/grub-2.06.tar.xz"
+  mirror "https://mirrors.ocf.berkeley.edu/gnu/grub/grub-2.06.tar.xz"
   sha256 "b79ea44af91b93d17cd3fe80bdae6ed43770678a9a5ae192ccea803ebb657ee1"
   license "GPL-3.0-or-later"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "gcc@11" => [:build, :test]
-  depends_on "gettext" => :build
   depends_on "help2man" => :build
   depends_on "i686-elf-binutils" => :build
   depends_on "i686-elf-gcc" => [:build, :test]
-  depends_on "make" => :build
   depends_on "objconv" => :build
   depends_on "texinfo" => :build
+  depends_on "gettext"
+  depends_on "xorriso"
   depends_on "xz"
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
@@ -33,11 +31,6 @@ class I686ElfGrub < Formula
         --libdir=#{lib}/#{target}
         --with-platform=pc
         --program-prefix=#{target}-
-        TARGET_CC=#{target}-gcc
-        TARGET_OBJCOPY=#{target}-objcopy
-        TARGET_STRIP=#{target}-strip
-        TARGET_NM=#{target}-nm
-        TARGET_RANLIB=#{target}-ranlib
       ]
 
       system "../configure", *args
@@ -58,6 +51,6 @@ class I686ElfGrub < Formula
     C
     system Formula["#{target}-gcc"].bin/"#{target}-gcc", "-c", "-o", "boot", "boot.c"
     assert_match "0",
-      shell_output("#{bin}/#{target}-grub-file --is-x86-multiboot boot; echo $?")
+      shell_output("#{bin}/#{target}-grub-file --is-x86-multiboot boot; echo -n $?")
   end
 end
